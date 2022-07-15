ef63d51139acdb6719123b70de04faef3e6f46eb
dylanchou <dylanchouxd@qq.com>
2022-07-15 11:56:53 +0800

    feat: 更新Ts的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,14 +69,14 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error',
+      'warn',
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
       },
     ],
     'no-unused-vars': [
-      'error',
+      'warn',
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
